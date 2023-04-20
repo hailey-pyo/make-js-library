6d111a27dc2bc8dabc41094253c33ab49a4e90f8
hailey-pyo <haeyoung9627@gmail.com>
2023-04-20 17:57:02 +0900

    _find_index, _some, _every 만들기 min, max, min_by, max_by 만들기 group_by, push 만들기 count_by, inc 만들기

--- js/made.js
+++ js/made.js
@@ -11,13 +11,13 @@ function _filter(list, predicate) {
     return new_list;
 }
 
-function _map(list, mapper) {
-    var new_list = [];
-    _each(list, function(val){
-        new_list.push(mapper(val))
-    });
-    return new_list;
-}
+// function _map(list, mapper) {
+//     var new_list = [];
+//     _each(list, function(val){
+//         new_list.push(mapper(val))
+//     });
+//     return new_list;
+// }
 
 //아래에 새롭게 만듦
 // function _each(list, iter) {
@@ -106,10 +106,159 @@ function _keys(obj){
 
 // _each의 외부 다형성 높이기 - _each에 null 넣어도 에러안나게
 // key-value쌍 넣어도 에러안나게   
+// function _each(list, iter) {
+//     var keys = _keys(list);
+//     for(var i=0, len  = keys.length; i< len; i++) {
+//         iter(list[keys[i]])
+//     }
+//     return list;
+// }
+
+
+// 컬렉션중심 프로그래밍! 
+//1. 수집하기 - map
+//_values, _pluck 만들기
+
+// function _values(data) {
+//     return _map(data, function(val){ return val; })
+// }
+//->_identity사용하여 수정가능
+function _values(data) {
+    return _map(data, _identity);
+}
+function _identity(val){
+    return val;
+}
+
+var _values = _map(_identity);//_map을 커링으로 구현했놨기때문에, 이렇게 표현가능
+
+// function _pluck(data, key) {
+//     return _map(data, function(obj){
+//         return obj[key];
+//     })
+// }
+//->_get사용하여 수정가능
+function _pluck(data, key) {
+    return _map(data, _get(key))
+}
+
+
+//2. 거르기 - filter
+//_reject, _compact 만들기
+
+// function _reject(data, predi) {
+//     return _filter(data, function(val){
+//         return !predi(val);
+//     });
+// }
+//->_negate사용하여 수정가능
+function _reject(data, predi) {
+    return _filter(data, _negate(predi));
+}
+
+function _negate(func) {
+    return function(val){
+        return !func(val);
+    }
+}
+
+var _compact = _filter(_identity);
+
+
+//3. 찾아내기 - find
+//_find_index, _some, _every 만들기
+var _find = _curryr(function(list, predi) {
+    var keys = _keys(list);
+    for(var i=0, len  = keys.length; i< len; i++) {
+        var val =list[keys[i]];
+        if(predi(val)) return val;
+    }
+});
+var _find_index = _curryr(function(list, predi) {
+    var keys = _keys(list);
+    for(var i=0, len  = keys.length; i< len; i++) {
+        if(predi(list[keys[i]])) return i;
+    }
+    return -1;
+});
+function _some(data, predi){
+    return _find_index(data, predi || _identity) != -1;
+}
+function _every(data, predi){
+    return _find_index(data, _negate(predi || _identity)) == -1;
+}
+
+
+//4. 접기,축약 - reduce
+//min, max, min_by, max_by 만들기
+//group_by, push 만들기
+//count_by, inc 만들기
+function _min(data){
+    return _reduce(data, function(a, b){
+        return a < b ? a: b;
+    });
+}
+function _max(data){
+    return _reduce(data, function(a, b){
+        return a > b ? a: b;
+    });
+}
+function _min_by(data, iter){
+    return _reduce(data, function(a, b){
+        return iter(a) < iter(b) ? a: b;
+    });
+}
+function _max_by(data, iter){
+    return _reduce(data, function(a, b){
+        return iter(a) > iter(b) ? a: b;
+    });
+}
+var _min_by = _curryr(_min_by),
+    _max_by = _curryr(_max_by);
+
+function _group_by(data, iter){
+    return _reduce(data, function(grouped, val){
+        // var key = iter(val);
+        // (grouped[key] = grouped[key] || []).push(val);
+        //return grouped;
+        return _push(grouped, iter(val), val);
+    }, {});
+}
+
+function _push(obj, key, val){
+    (obj[key] = obj[key] || []).push(val);
+    return obj;
+}
+
+function _count_by(data, iter){
+    return _reduce(data, function(count, val){
+        return _inc(count, iter(val));
+    }, {});
+}
+function _inc(count, key){
+    count[key] ? count[key]++ : count[key]=1;
+    return count;
+}
+
+//_each _map 다시수정
+//콜백함수 인자에 key 추가해줌
 function _each(list, iter) {
     var keys = _keys(list);
     for(var i=0, len  = keys.length; i< len; i++) {
-        iter(list[keys[i]])
+        iter(list[keys[i]], keys[i]);
     }
     return list;
 }
+function _map(list, mapper) {
+    var new_list = [];
+    _each(list, function(val, key){
+        new_list.push(mapper(val, key));
+    });
+    return new_list;
+}
+var _pairs = _map((val, key) => [key, val]);
+
+//응용
+//_count_by _reject 커링필요
+var _count_by = _curryr(_count_by);
+var _reject = _curryr(_reject);
